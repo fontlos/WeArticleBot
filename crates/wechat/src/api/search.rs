a53d0c108ef81c7e58d171d2c506a9c1dc4605ef
Fontlos <fontlos@fontlos.com>
2026-06-03 21:12:57 +0800

    feat: Add args for `search` in `wechat`

--- crates/wechat/src/api/search.rs
+++ crates/wechat/src/api/search.rs
@@ -1,24 +1,27 @@
+use bytes::Bytes;
+
 use crate::error::Result;
 use crate::session::Session;
 
 impl Session {
-    pub async fn search(&self) -> Result<()> {
+    pub async fn search(&self, key: &str, page: usize) -> Result<Bytes> {
         let url = "https://mp.weixin.qq.com/cgi-bin/searchbiz";
+        let token = &self.token.load();
+        let size = 10;
+        let begin = (page - 1) * size;
         let query = [
             ("action", "search_biz"),
-            ("begin", "{BEGIN}"),
-            ("count", "{SIZE}"),
-            ("query", "{KEYWORD}"),
-            ("token", "{TOKEN}"),
+            ("begin", &begin.to_string()),
+            ("count", &size.to_string()),
+            ("query", key),
+            ("token", token),
             ("lang", "zh_CN"),
             ("f", "json"),
             ("ajax", "1"),
         ];
         let res = self.client.get(url).query(&query).send().await?;
-        println!("Url: {}", url);
-        let text = res.text().await?;
-        println!("Search response: {}", text);
-        Ok(())
+        let bytes = res.bytes().await?;
+        Ok(bytes)
     }
 
     pub async fn list(&self) -> Result<()> {
