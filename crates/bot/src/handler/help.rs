a5281fe0d2ab7f2e1aff4103942173fbb546f71c
Fontlos <fontlos@fontlos.com>
2026-08-15 14:03:24 +0800

    refactor: Use post as help message'

--- crates/bot/src/handler/help.rs
+++ crates/bot/src/handler/help.rs
@@ -3,6 +3,20 @@ use lark::api::Message;
 use crate::context;
 
 pub async fn reply(chat_id: &str, text: &str) {
-    let msg = Message::to_chat(chat_id).text(text);
+    let shell = format!("```shell\n{}\n```", text);
+    let md = serde_json::json!({
+        "zh_cn": {
+            "title": "命令解析错误",
+            "content": [
+                [
+                    {
+                        "tag": "md",
+                        "text": shell,
+                    }
+                ]
+            ],
+        }
+    });
+    let msg = Message::to_chat(chat_id).post(md.to_string());
     context::lark().send_message(msg).await.unwrap();
 }
